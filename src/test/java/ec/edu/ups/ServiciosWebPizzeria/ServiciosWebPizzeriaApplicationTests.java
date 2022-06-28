1603f6df1396a247b1552f7ff08444f6dbbcc96e
jodacoga <johancordovag@gmail.com>
2022-06-28 09:41:18 -0500

    First commit

---
+++ src/test/java/ec/edu/ups/ServiciosWebPizzeria/ServiciosWebPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.ServiciosWebPizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosWebPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
